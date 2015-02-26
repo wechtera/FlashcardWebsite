b2aec1d539c305c1aa6008a5bdf3810a922ffbb7
Adam Wechter <wechtera@gmail.com>
2015-02-25 20:07:21 -0500

    Div selection studyDeck done todo formatting

--- flashcards/client/views/study_deck/studydeck.js
+++ flashcards/client/views/study_deck/studydeck.js
@@ -1,13 +1,39 @@
+var dependencyVar = new Deps.Dependency;
+var currentFocus =  null;
+
 
 if(Meteor.isClient) {
 	Template.studydeck.helpers({
 		getDecks: function() {
 			return flashDeck.find().fetch();
 		}
-
-	
+		/*
+		getDecksMine: function() {
+			return flashDeck.find(userId: getCurrentUser).fetch();
+		}
+		*/
 	});
+	Template.studydeck.events({
+		'click .deckListDivs': function(event) {
+			var id = event.currentTarget.id;
+			var ele = event.currentTarget;
+			console.log(id);
+			if(currentFocus === null) {
+				currentFocus = id;
+				ele.className = "listDivFocused";
+
+			}
+			else {
+				//Need to clear focus events from other div
+				document.getElementById(currentFocus).className = "deckListDivs";
+				currentFocus = id;
+				ele.className = "listDivFocused";
+			}
+		}
+	});
+
 	Template.studydeck.rendered = function() {
 		$('select').Minimalistbox();
+		
 	}
 }
